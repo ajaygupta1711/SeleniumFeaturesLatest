3039a5bc929cd80ed58cad6353d87ac72b4d9035
Ajay <ajay.gupta1711@gmail.com>
2022-05-06 19:53:48 +0530

    Basic Authentication and Console Logs Capture

---
+++ src/BasicAuthentication.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.net.URI;
+import java.util.function.Predicate;
+
+public class BasicAuthentication {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium with Java\\Software Installs_IMP\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		// Predicate, Consumer
+		Predicate <URI> uriPredicate = uri -> uri.getHost().contains("httpbin.org");
+		
+		((HasAuthentication)driver).register(uriPredicate, UsernameAndPassword.of("foo", "bar"));
+		driver.get("http://httpbin.org/basic-auth/foo/bar");
+				
+	}
+
+}
---
+++ src/ConsoleLogsCapture.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+public class ConsoleLogsCapture {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium with Java\\Software Installs_IMP\\chromedriver.exe");	
+		ChromeDriver driver = new ChromeDriver();
+		
+		// Listeners - OnTestFailure
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.linkText("Browse Products")).click();
+		driver.findElement(By.partialLinkText("Selenium")).click();
+		driver.findElement(By.cssSelector(".add-to-cart")).click();
+		driver.findElement(By.linkText("Cart")).click();
+		driver.findElement(By.id("exampleInputEmail1")).clear();
+		driver.findElement(By.id("exampleInputEmail1")).sendKeys("2");
+		
+		LogEntries entry = driver.manage().logs().get(LogType.BROWSER); // Get LogEntries Object
+		List<LogEntry> logs = entry.getAll(); // LogEntry object  - getAll method return all logs in the list
+		
+		for(LogEntry e : logs) // Iterating through list and printing each log message 
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+}
